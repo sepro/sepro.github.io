97c99c43789fa61bdc4273d3635df58c81f5fa40
sepro <sebastian.proost@gmail.com>
2020-11-15 11:18:52 +0100

    added yaml files

--- src/pages/experience.js
+++ src/pages/experience.js
@@ -1,19 +1,30 @@
 import React from "react"
 import Layout from "../components/layout"
-import Item from "../components/item"
 import ExperienceData from "../../content/experience.yaml"
+
+import SchoolIcon from '@material-ui/icons/School';
+import WorkIcon from '@material-ui/icons/Work';
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component'
+import 'react-vertical-timeline-component/style.min.css'
 
 export default props => {
 
   const experience = ExperienceData.map((item, index) => (
-    <Item
+      <VerticalTimelineElement
       key={index}
-      name={item.role}
-      when={item.when}
-      where={item.where}
-      org={item.company}
-      moreInfo={item.moreInfo}
-    />
+      className="vertical-timeline-element--work"
+      contentStyle={{ background: 'rgb(230, 138, 0)', color: '#fff' }}
+      contentArrowStyle={{ borderRight: '7px solid  rgb(230, 138, 0)' }}
+      date={item.when}
+      iconStyle={{ background: 'rgb(230, 138, 0)', color: '#fff' }}
+      icon={<WorkIcon />}
+    >
+      <h3 className="vertical-timeline-element-title">{item.role}</h3>
+      <h4 className="vertical-timeline-element-subtitle">{item.company} - {item.where}</h4>
+      <p>
+      {item.moreInfo}
+      </p>
+    </VerticalTimelineElement>
   ))
 
   return (
@@ -21,7 +32,9 @@ export default props => {
       pageTitle="Experience"
       pageDescription="Learn more about my experience"
     >
+      <VerticalTimeline layout="1-column-left">
       {experience}
+      </VerticalTimeline>
     </Layout>
   )
 }
