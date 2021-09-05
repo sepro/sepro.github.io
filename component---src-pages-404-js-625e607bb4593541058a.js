0dc195b86ffb3fcdbed8ec57a1d980125b7a666c
sepro <sepro@users.noreply.github.com>
2021-09-05 17:12:13 +0000

    deploy: 872524041cc051caec860f7ff62dd2f4271ce10e

--- component---src-pages-404-js-625e607bb4593541058a.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),o=a.n(n),r=a("Bl7J");t.default=function(e){return o.a.createElement(r.a,{pageTitle:"Page Not Found!",pageDescription:""},o.a.createElement("p",null,"Ooops! Wrong turn!"),o.a.createElement("p",null,"Head back to the ",o.a.createElement("a",{href:"/"},"Home")," page"))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-625e607bb4593541058a.js.map
